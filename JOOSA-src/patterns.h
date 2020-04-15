326586cceccb323f3a4fbcd5d5ae7016776ea248
Hieu Chau Nguyen <hieu.c.nguyen@mail.mcgill.ca>
2020-04-15 00:00:53 -0400

    Add HieuChau's contribution

--- JOOSA-src/patterns.h
+++ JOOSA-src/patterns.h
@@ -92,9 +92,84 @@ int simplify_goto_goto(CODE **c)
   }
   return 0;
 }
+
+/* istore x
+ * iload x
+ * ----->
+ * (removed both)
+ * Keep the element in stack for next usage.
+*/
+int remove_storeloadcouple(CODE **c){
+  int x1, x2;
+  if(is_istore(*c, &x1) && is_iload(next(*c),&x2)){
+    if(x1 == x2){
+      return replace(c,2, NULL);
+    }
+  }
+  return 0;
+}
+/*
+ * ldc a
+ * iadd
+ * ldc b
+ * iadd
+ * ....
+ * ldc n
+ * iadd
+ * -------->
+ * ldc_int (a+b+...+n)
+ * iadd
+ */
+ int add_constants(CODE **c){
+   int inc = 0;
+   int i;
+   int loop = 0;
+   CODE *p = *c;
+   while(is_ldc_int(p, &i) && is_iadd(next(p))){
+     ++loop;
+     inc += i;
+     p = next(next(p));
+   }
+   if(loop == 0) return 0;
+   return replace(c,loop*2,makeCODEldc_int(inc,
+                      makeCODEiadd(NULL)
+   ));
+ }
+
+/*
+ * ldc a
+ * imul
+ * ldc b
+ * imul
+ * ....
+ * ldc n
+ * imul
+ * -------->
+ * ldc_int (a+b+...+n)
+ * imul
+ */
+ int mul_constants(CODE **c){
+   int inc = 0;
+   int i;
+   int loop = 0;
+   CODE *p = *c;
+   while(is_ldc_int(p, &i) && is_imul(next(p))){
+     inc *= i;
+     p = next(next(p));
+   }
+   if(loop == 0) return 0;
+
+   return replace(c,loop*2,makeCODEldc_int(inc,
+                      makeCODEimul(NULL)
+   ));
+ }
 void init_patterns(void) {
 	ADD_PATTERN(simplify_multiplication_right);
 	ADD_PATTERN(simplify_astore);
 	ADD_PATTERN(positive_increment);
 	ADD_PATTERN(simplify_goto_goto);
+	ADD_PATTERN(remove_storeloadcouple);
+	ADD_PATTERN(add_constants);
+	ADD_PATTERN(mul_constants);
+
 }
